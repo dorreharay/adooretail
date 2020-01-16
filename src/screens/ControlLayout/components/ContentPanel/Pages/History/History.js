99ccc9dbf57618ea972c08729ed9920fd4593595
Andrii Didkivskyi <dorreharay@gmail.com>
2020-01-16 18:49:49 +0200

    changes

--- src/screens/ControlLayout/components/ContentPanel/Pages/History/History.js
+++ src/screens/ControlLayout/components/ContentPanel/Pages/History/History.js
@@ -1,108 +1,17 @@
 import React, { useState, useRef, Fragment, } from 'react'
 import { View, Text, ScrollView, TouchableOpacity, Animated, Easing, } from 'react-native'
+import { useSelector, } from 'react-redux'
 import FastImage from 'react-native-fast-image'
 import styles from './styles'
 import Collapsible from 'react-native-collapsible'
+let moment = require('moment');
+moment.locale('uk');
+
+import { currentAccountSelector, } from '@selectors'
 
 function History(props) {
-  const [days, setDays] = useState([
-    {
-      date: 'Вівторок 21.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Середа 22.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Четвер 23.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: "П'ятниця 24.02.19",
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Субота 25.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Неділя 26.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Вівторок 21.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Середа 22.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Четвер 23.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: "П'ятниця 24.02.19",
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Субота 25.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-    {
-      date: 'Неділя 26.02.19',
-      total: 2341,
-      receipts: [{}, {}, {}],
-      employees: [
-        'Андрій', 'Ігор', 'Ростік',
-      ]
-    },
-  ])
+  const currentAccount = useSelector(currentAccountSelector)
+
   const [expandedIndex, setExpandedIndex] = useState(null)
   const [spinValue] = useState(new Animated.Value(1))
 
@@ -150,7 +59,7 @@ function History(props) {
       contentContainerStyle={{ paddingBottom: 10000, }}
       scrollEnabled={expandedIndex === null}
     >
-      {days.map((day, index) => (
+      {currentAccount.localSessions.map((day, index) => (
         <Fragment key={index}>
           <TouchableOpacity
             style={styles.dayHeader}
@@ -161,9 +70,9 @@ function History(props) {
               style={{ width: 20, height: 20, marginRight: 20, }}
               source={require('@images/session_process.png')}
             />
-            <Text style={styles.dayHeaderDate}>{day.date}</Text>
-            <Text style={styles.dayHeaderTotal}>Всього за зміну: {day.total}</Text>
-            <Text style={styles.dayHeaderEmployees}>Працівників на зміні: {day.employees.length}</Text>
+            <Text style={styles.dayHeaderDate}>{moment(day.startTime).format('dddd DD.MM.YY')}</Text>
+            <Text style={styles.dayHeaderTotal}>Всього за зміну: 0</Text>
+            <Text style={styles.dayHeaderEmployees}>Працівників на зміні: 0</Text>
             <View style={styles.dayHeaderIcon}>
               <AnimatedImage
                 style={[{ width: 15, height: 15 }, expandedIndex === index && { transform: [{ rotate: spin }] }]}
@@ -203,16 +112,15 @@ function History(props) {
                 contentContainerStyle={{ flexDirection: 'row', flexWrap: 'wrap', paddingBottom: 30, }}
                 scrollEnabled={expandedIndex === index}
               >
-                {[{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},].map((item, index) => (
+                {day.receipts.map((item, index) => (
                   <View style={styles.historyInstance}>
-                    <Text style={styles.receiptTime}>12:31</Text>
-                    <Text style={styles.receiptItem}>Лате 250 мл - 25 грн</Text>
-                    <Text style={styles.receiptItem}>Еспресо 200 мл - 25 грн</Text>
-                    {/* <Text style={styles.receiptItem}>Еспресо 250 мл - 25 грн</Text>  */}
-                    <Text style={styles.receiptTotal}>Всього: 340 грн</Text>
+                    <Text style={styles.receiptTime}>{moment(item.transaction_time_end).format('hh:mm')}</Text>
+                    {item.receipt.map(elem => (
+                      <Text style={styles.receiptItem}>{elem.title} - {elem.price * elem.quantity} грн</Text>
+                    ))}
+                    <Text style={styles.receiptTotal}>Всього: {item.receipt.reduce((accumulator, currentValue) => accumulator + (currentValue.price * currentValue.quantity), false)} грн</Text>
                   </View>
                 ))}
-
               </ScrollView>
             </View>
           </Collapsible>
